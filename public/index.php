ffc6384fbf5c8f95d9cdbde716b3483cb23d1b57
Dumitru Stefan-Fernando <dumitrustefanfernando@outlook.com>
2026-05-26 00:56:29 +0200

    Add services API with Fetch, selection, validation and appointment summary

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
-use Controllers\LoginController;
+use Controllers\APIController;
 use Controllers\CitaController;
+use Controllers\LoginController;
 use MVC\Router;
-
 $router = new Router();
 
 
@@ -30,6 +30,9 @@ $router->get('/mensaje', [LoginController::class, 'mensaje']);
 
 // AREA PRIVADA
 $router->get('/cita', [CitaController::class, 'index']);	
+
+// API de Citas
+$router->get('/api/servicios', [APIController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
